685851db10fe6c5d8e78f03ca166219799792b5e
Naty Ramirez <nataliaramirez1820@gmail.com>
2022-09-25 12:06:39 -0300

    proyecto final Ramirez Natalia

---
+++ js/main.js
@@ -0,0 +1,129 @@
+//registrar usuario
+
+class User {
+  constructor(nombre, apellido, email, password){
+    this.nombre= nombre;
+    this.apellido=apellido;
+    this.email= email;
+    this.password=password;
+  }
+}
+
+//expresiones regulares 
+const expresiones = {
+  password: /^.{4,12}$/, 
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+
+}
+
+let usuarioCreado = [];
+
+Verify ()
+
+
+//evento de logueo
+
+
+let btnSubmit=document.getElementById('submit');
+btnSubmit.onclick=()=>{
+let nombreIngresado=document.getElementById('nombreUsuario').value;
+
+let apellidoIngresado=document.getElementById('apellidoUsuario').value;
+
+let emailIngresado=document.getElementById('emailUsuario').value;
+const validarEmail= (expresiones.correo.test(emailIngresado))? true : false
+validarEmail? document.getElementById('val-email').innerHTML=`Email Válido`:  swal({
+  title: "Algo salio mal",
+  text: "Ingresa datos válidos",
+  icon: "error",
+  button: "reintentar",
+});
+
+
+let passwordIngresado= document.getElementById('passwordUsuario').value;
+const validarPass= (expresiones.password.test(passwordIngresado))? true : false 
+validarPass? document.getElementById('val-pass').innerHTML=`Password Válido`: document.getElementById('val-pass').innerHTML=`debe contener mínimo 4 caracteres y máximo 8` 
+
+
+let validarCampos=((nombreIngresado!="")&&(apellidoIngresado!="")&&(emailIngresado!="")&&(passwordIngresado!=""))? true : false
+validarCampos? swal({
+  title: "Usuario crerado",
+  text: "Bienvenido",
+  icon: "success",
+  button: "vamos!",
+}) : document.getElementById('bienvenidoUsuario').innerHTML=`Ingresar datos válidos`
+
+
+
+let objetoUsuario ={nombre: nombreIngresado, apellido: apellidoIngresado, email: emailIngresado, password: passwordIngresado }
+usuarioCreado.push( new User (objetoUsuario));
+
+ 
+save()
+
+}
+// guardamos en localStorage
+function save() {
+localStorage.setItem("listaUsuario",JSON.stringify (usuarioCreado));
+  
+}
+
+//verificacion en LS
+function Verify () {
+  let arrayAuxiliar=JSON.parse(localStorage.getItem("listaUsuario"));
+
+  if(arrayAuxiliar){
+
+   for(elemento of arrayAuxiliar )
+        {
+            usuarioCreado.push(new User(elemento));
+            
+        }
+        
+}else{
+        Swal.fire({
+  title: 'Usuario Inexistente!',
+   icon: 'error',
+  width: 500,
+  timer:'10000',
+  padding: '3em',
+  color: '#716add',
+  background: '#fffa ',
+  backdrop: `rgba(0,0,123,0.4)`
+    })
+   
+}
+}
+
+
+
+
+
+//validacion form
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
+
+
+Toastify({
+  text: "Bienvenidos",
+  className: "info",
+  style: {
+    background: "linear-gradient(to right, #2a2b38, #5e6681)",
+  }
+}).showToast();
